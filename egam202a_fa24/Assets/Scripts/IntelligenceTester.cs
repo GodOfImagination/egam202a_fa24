dc4fc175dbcd3882b95b63684012a01d60a6b0bf
GodOfImagination <kurzawa.jaden@gmail.com>
2024-09-19 08:12:26 -0700

    Created Git

---
+++ egam202a_fa24/Assets/Scripts/IntelligenceTester.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntelligenceTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float JadenVariable = 1f;
+        JadenVariable += 1f;
+        Debug.Log(JadenVariable);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
